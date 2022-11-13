db06ae28600edff6e504d341ff00efca8b35050c
Master-Leo <emillio3198@gmail.com>
2022-11-12 22:02:27 -0800

    'edited var'

--- Leaflet-Part1/challenge.js
+++ Leaflet-Part1/challenge.js
@@ -4,28 +4,28 @@ var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 })
 
 // Create a base layer that holds both map layers.
-let baseMaps = {
+var baseMaps = {
     Streets: street,
 };
   
   // Create the map object with center, zoom level and default layer.
-let map = L.map('map', {
+var map = L.map('map', {
     center: [39.5, -98.5],
     zoom: 5,
     layers: [street],
 });
 
-let Earthquakes = new L.LayerGroup();
+var Earthquakes = new L.LayerGroup();
 
-let overlays = {
+var overlays = {
     "Earthquakes": Earthquakes,
 };
-
   
   // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps, overlays).addTo(map);
   
 var queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+
   // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function(data) {
     console.log(data);
@@ -38,24 +38,23 @@ d3.json(queryUrl).then(function(data) {
             radius: getR(feature.properties.mag)
         };
     function getFill(magnitude){
-        // f = chroma.scale(['green','orange','red']).colors(6);
         if (magnitude >= 5) {
-            return '#ee0000';
+            return '#FF0D0D';
           }
           if (magnitude >= 4) {
-            return '#ee3400';
+            return '#FF4E11';
           }
           if (magnitude >= 3) {
-            return '#ee4f00';
+            return '#FF8E15';
           }
           if (magnitude >= 2) {
-            return '#ee7300';
+            return '#FAB733';
           }
           if (magnitude >= 1) {
-            return '#afee00';
+            return '#D3DB40';
           }
           else
-            return '#5bee00';
+            return '#69B34C'
         }
     }
     function getR(magnitude) {
@@ -65,16 +64,43 @@ d3.json(queryUrl).then(function(data) {
         else
           return magnitude * 5;
     }
+    function colorScale(m){
+        return m >= 5 ? '#FF0D0D' :
+                m >= 4 ?'#FF4E11' :
+                  m >= 3 ? '#FF8E15' :
+                    m >= 2 ? '#FAB733' :
+                       m >= 1 ? '#D3DB40' :
+                          m >= 0 ? '#69B43C': '#69B43C';
+    }
     
     L.geoJson(data, {
         pointToLayer: function(feature, latlng) {
             console.log(data);
             return L.circleMarker(latlng);
         },
-        style: style
+        style: style,
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<h3>${feature.properties.title}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+        },
     }).addTo(Earthquakes);
 
-    // First Layer: Earthquake 
-   Earthquakes.addTo(map);
+    Earthquakes.addTo(map);
+ 
+    var legend = L.control({position: "bottomright"});
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+            magnitude = [0, 1, 2, 3, 4, 5];
+            labels = [];
+
+        for (var i = 0; i < magnitude.length; i++) {
+            div.innerHTML +=
+            '<i style="background:' + colorScale(magnitude[i])+ '"></i> ' +
+                magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+
+    // Finally, displaying  color legend to the map.
+    legend.addTo(map);
 
 }); 
